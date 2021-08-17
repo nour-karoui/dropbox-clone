9449772f0fd7af838663b7f6fd6f1d2b36c92f9a
nour-karoui <nkaroui1998@gmail.com>
2021-08-17 16:59:41 +0100

    Updates

---
+++ precache-manifest.b72f77aa60e4a4e74589cb6dce42cc47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365538f3af2b98b3477cf2ac68f9bc1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b735569568637a1f007",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "b903c872324995083234",
+    "url": "./static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "3b735569568637a1f007",
+    "url": "./static/js/2.a7d64fe5.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.a7d64fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b903c872324995083234",
+    "url": "./static/js/main.1bd86a3e.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
